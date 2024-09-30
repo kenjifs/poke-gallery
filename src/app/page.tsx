b971d242bb8392a7612e6c0601d0052db8d57640
kenjifs <kenjiferdiansantosa@gmail.com>
2024-09-30 16:45:38 +0700

    use auht for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div className="flex w-48 flex-col" key={image.id + "-" + index}>
+          <img src={image.url} alt="" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div className="flex w-48 flex-col" key={image.id + "-" + index}>
-            <img src={image.url} alt="" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please Sign In Above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
