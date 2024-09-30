bce36928dad13cbb9080f6fb9b2cfefa7a4de0f6
kenjifs <kenjiferdiansantosa@gmail.com>
2024-09-30 13:25:07 +0700

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/sCxHDYy8HBnugURiJlDOHQpIJUefjrhS7qk8T5DNZnuC94Bc",
+  "https://utfs.io/f/sCxHDYy8HBnu1mqXd3yVdhNalPUm49iRTI7gxZMOpjnfs5uL",
+  "https://utfs.io/f/sCxHDYy8HBnuyVsmQ8XbIQEHYD8MpJn0SWOx6GT9Xfe2ouLg",
+  "https://utfs.io/f/sCxHDYy8HBnu4NR8PEILiY0UB1trl5XK8wqQsxvSneFpOzPZ",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div>Poke Gallery</div>
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div className="w-48" key={image.id}>
+            <img src={image.url} alt="" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
